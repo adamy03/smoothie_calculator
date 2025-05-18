a65bb1539c0fc5fcb4cdb4b8fd75f88b7b2bd414
Adam Yang <66138604+adamy03@users.noreply.github.com>
2025-05-17 22:06:04 -0400

    json stuff

--- smoothieServer.js
+++ smoothieServer.js
@@ -4,14 +4,29 @@ const fs = require("fs");
 const path = require("path");
 const bodyParser = require("body-parser"); /* To handle post parameters */
 const app = express(); 
-const portNumber = 5000;
+const portNumber = 5005;
+const fruits = [
+    "strawberry", 
+    "banana", 
+    "pear", 
+    "blackberry", 
+    "kiwi", 
+    "pineapple", 
+    "fig", 
+    "passionfruit", 
+    "raspberry", 
+    "mango", 
+    "blueberry", 
+    "apple", 
+    "peach"
+]
 
 require("dotenv").config({
    path: path.resolve(__dirname, ".env"),
 });
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const { url } = require("inspector");
-
+const { emitWarning } = require("process");
 
 app.set("view engine", "ejs");
 app.set("views", path.resolve(__dirname, "templates"));
@@ -19,7 +34,6 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.static('templates'));
 
 const homeURL = `http://localhost:${portNumber}`
-
 
 /* Command Line Interface */
 process.stdin.setEncoding("utf8");
@@ -33,37 +47,60 @@ app.get("/smoothieMaker", (req, res) => {
     res.render("smoothieMaker", {url: homeURL+"/smoothieMaker"});
 });
 
-
 app.post("/smoothieMaker", (req, res) => {
-    const {name} = req.body; // #TODO
-    (async () => {
-        const databaseName = process.env.MONGO_DB_NAME;
-        const collectionName = process.env.MONGO_COLLECTION
-        const uri = process.env.MONGO_CONNECTION_STRING;
-        const client = new MongoClient(uri, { serverApi: ServerApiVersion.v1 });
+    const {
+    strawberry, 
+    banana, 
+    pear, 
+    blackberry, 
+    kiwi, 
+    pineapple, 
+    fig, 
+    passionfruit, 
+    raspberry, 
+    mango, 
+    blueberry, 
+    apple, 
+    peach} = req.body
+    
+    let item = {strawberry, banana, pear, blackberry, kiwi, pineapple, fig, passionfruit, raspberry, mango, blueberry, apple, peach}
+
+    console.log(item);
+    // const fruits = JSON.parse(req.body.toString()).filter(f => f > 0);
+    // req.bodyforEach(element => {
+    //     console.log(element)
+    // });
+
+    // (async () => {
+    //     const databaseName = process.env.MONGO_DB_NAME;
+    //     const collectionName = process.env.MONGO_COLLECTION
+    //     const uri = process.env.MONGO_CONNECTION_STRING;
+    //     const client = new MongoClient(uri, { serverApi: ServerApiVersion.v1 });
      
-        try {
-            await client.connect();
-            const database = client.db(databaseName);
-            const collection = database.collection(collectionName);
+    //     try {
+    //         await client.connect();
+    //         const database = client.db(databaseName);
+    //         const collection = database.collection(collectionName);
             
-            const recipie = {}; //#TODO 
-            result = await collection.insertOne(recipie);
-        } catch (e) {
-            console.error(e);
-        } finally {
-            await client.close();
-            res.render("smoothieMaker", {
-                name: name,
-                email: email,
-                gpa: gpa,
-                background: background,
-            });
-        }
-    })();
+    //         const recipie = {}; //#TODO 
+    //         result = await collection.insertOne(recipie);
+    //     } catch (e) {
+    //         console.error(e);
+    //     } finally {
+    //         await client.close();
+    //         res.render("smoothieMaker", {
+    //             name: name,
+    //             email: email,
+    //             gpa: gpa,
+    //             background: background,
+    //         });
+    //     }
+    // })();
 
     
 });
+
+app.get("/api/")
 
 app.get("/smoothieGetter", (req, res) => {
     res.render("smoothieGetter", {
@@ -122,5 +159,7 @@ function getIngredientTable(ingredientsData) {
     
     tableStr += "</div>"
 }
+
+
 
 app.listen(portNumber);
